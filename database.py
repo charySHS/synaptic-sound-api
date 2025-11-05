db8d0e7f7980951dcd64b351548d867e590aca1a
charyshs <smkzachatac@gmail.com>
2025-11-04 21:04:57 -0800

    Fix get_db dependency injection

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 # Imports
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 import os
 
@@ -20,7 +20,7 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db: Session = SessionLocal()
     try:
         yield db
     finally:
